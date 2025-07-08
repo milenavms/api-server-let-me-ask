b4a8346591813a8a3a969da0e9ab775bba8f2e15
Milena <milenavasconcelos.info@gmail.com>
2025-07-07 23:52:27 -0400

    chore: add and configure Biome for code formatting and linting

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,25 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
-    serializerCompiler,
-    validatorCompiler,
-    type ZodTypeProvider 
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
 });
 
-app.setSerializerCompiler(serializerCompiler)
+app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-    return 'ok';
-})
+  return 'ok';
+});
 
 app.listen({
-    port: env.PORT,
-}).then(() => {
-    console.log(`HTTP server running on port ${process.env.PORT}`);
-})
+  port: env.PORT,
+});
